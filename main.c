8bbae6bf3072d3b2779b17445f5732882a919c23
AdrianPilko <50658445+AdrianPilko@users.noreply.github.com>
2023-04-10 16:13:00 +0100

    scoring and added

--- main.c
+++ main.c
@@ -21,6 +21,8 @@
 #include <input.h>
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
 
 IM2_DEFINE_ISR(isr) {}
 #define TABLE_HIGH_BYTE        ((unsigned int)0xD0)
@@ -31,6 +33,7 @@ IM2_DEFINE_ISR(isr) {}
 #define JUMP_POINT             ((unsigned char*)( (unsigned int)(JUMP_POINT_HIGH_BYTE*UI_256) + JUMP_POINT_HIGH_BYTE ))
 
 #define MAX_SPEED_SACESHIP_1  6
+#define FIRE_CUTTOUT 6
 
 extern unsigned char spaceship1_masked[];
 extern unsigned char spaceship2_masked[];
@@ -40,6 +43,11 @@ struct sp1_Rect full_screen = {0, 0, 32, 24};
 void plot(unsigned char x, unsigned char y)
 {
 	*zx_pxy2saddr(x,y) |= zx_px2bitmask(x);
+}
+
+void clearplot(unsigned char x, unsigned char y)
+{
+	*zx_pxy2saddr(x,y) &= 0;
 }
 
 void line(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1)
@@ -62,6 +70,45 @@ void line(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1
 	}
 }
 
+void vertline(unsigned char x0, unsigned char y0, unsigned char y1)
+{		
+	unsigned char i;
+	for (i = y1; i < y0; i++)
+	{
+		plot(x0,i);		
+	}
+}
+
+
+void clearVertline(unsigned char x0, unsigned char y0, unsigned char y1)
+{	
+	unsigned char i;
+	for (i = y1; i < y0; i++)
+	{
+		clearplot(x0,i);		
+	}
+}
+
+void printScores(struct sp1_pss* pss, int score, int highScore, short lives)
+{
+	char buffer[7];
+	sp1_SetPrintPos(pss, 0, 0);   
+	sp1_PrintString(pss, "1UP Score--SPACE-WAR--High Score");
+	sp1_SetPrintPos(pss, 1, 0);   
+	sp1_PrintString(pss, "           Lives                ");
+	
+	
+	sprintf(buffer, "%d", score);
+	sp1_SetPrintPos(pss, 1, 3);   
+	sp1_PrintString(pss, buffer);
+	sprintf(buffer, "%d", highScore);
+	sp1_SetPrintPos(pss, 1, 25);   	
+	sp1_PrintString(pss, buffer);
+	sp1_SetPrintPos(pss, 1, 17);   
+	sprintf(buffer, "%d", lives);
+	sp1_PrintString(pss, buffer);
+}
+
 unsigned char colour;
 unsigned char cmask;
 
@@ -74,6 +121,13 @@ void colourSpr(unsigned int count, struct sp1_cs *c)
 
 int main()
 {
+  int score;
+  int highscore;
+  int dead;  
+  short lives;
+  
+  unsigned char countdownTillFireAvailable;
+  unsigned char overWriteLine;
   unsigned char c;
   
   struct sp1_ss  *spaceship1;
@@ -82,10 +136,14 @@ int main()
   unsigned char sp1XPos;
   short sp1XPosInc;
   short sp1YPosInc;
+  short sp2XPosInc;
+  short sp2YPosInc;
   
   unsigned char sp2XPos;
   unsigned char sp1YPos;
   unsigned char sp2YPos;
+  unsigned char sp1XPosPrev;
+  unsigned char sp1YPosPrev;
   unsigned char fire;
   
 
@@ -114,52 +172,148 @@ int main()
   colour = (INK_RED | PAPER_BLACK);   
   cmask = SP1_AMASK_INK & SP1_AMASK_PAPER; 
   sp1_IterateSprChar(spaceship2, colourSpr);
+  
+	  
+  struct sp1_pss pss = {
+	   &full_screen,   // print confined to this rectangle
+	   SP1_PSSFLAG_INVALIDATE | SP1_PSSFLAG_XWRAP | SP1_PSSFLAG_YINC | SP1_PSSFLAG_YWRAP,
+	   0,    // current x
+	   0,    // current y
+	   0,    // attribute mask - overwrite underlying colour
+	   INK_BLACK | PAPER_WHITE,
+	   0,    // sp1_update* must be consistent with x,y
+	   0    // visit function (set to zero)
+  };
+  highscore = 0;
+  while (1)  // main game loop
+  {
+	sp1_Initialize( SP1_IFLAG_MAKE_ROTTBL | SP1_IFLAG_OVERWRITE_TILES | SP1_IFLAG_OVERWRITE_DFILE,
+                  INK_YELLOW | PAPER_BLACK,' ' );
+				  
+    sp1_Invalidate(&full_screen);
+ 
+    spaceship1 = sp1_CreateSpr(SP1_DRAW_MASK2LB, SP1_TYPE_2BYTE, 2, (int)spaceship1_masked, 0);
+    spaceship2 = sp1_CreateSpr(SP1_DRAW_MASK2LB, SP1_TYPE_2BYTE, 2, (int)spaceship2_masked, 0);
 
-  sp1XPos=10;
-  sp2XPos=100;
-  sp1YPos=160;
-  sp2YPos=20;
-  sp1XPosInc = 1;
-  sp1YPosInc = 1;
-   
-  while(1) {
-	c = in_inkey();
+    sp1_AddColSpr(spaceship1, SP1_DRAW_MASK2RB, SP1_TYPE_2BYTE, 0, 0);
+    colour = (INK_CYAN | PAPER_BLACK);   
+    cmask = SP1_AMASK_INK & SP1_AMASK_PAPER; 
+    sp1_IterateSprChar(spaceship1, colourSpr);
+	  
+    sp1_AddColSpr(spaceship2, SP1_DRAW_MASK2RB, SP1_TYPE_2BYTE, 0, 0);
+    colour = (INK_RED | PAPER_BLACK);   
+    cmask = SP1_AMASK_INK & SP1_AMASK_PAPER; 
+    sp1_IterateSprChar(spaceship2, colourSpr);
+  
+	// initialise everything else  
+	sp1XPos=10;
+	sp2XPos=100;
+	sp1YPos=160;
+	sp2YPos=20;
+	sp1XPosInc = 1;
+	sp1YPosInc = 1;
+	sp2XPosInc = 4;
+	sp2YPosInc = 4;	
+	countdownTillFireAvailable = FIRE_CUTTOUT;
 	
-	switch (c)
+	score = 0;
+	dead = 0;   /// dead = 1 not dead = 0;
+	lives = 3;
+	 
+	printScores(&pss, score, highscore, lives);
+	   
+	while(dead == 0) 
 	{
-		case 'o' : sp1XPosInc-=1; break;
-		case 'p' : sp1XPosInc+=1; break;
-		case 'q' : sp1YPosInc-=1; break;
-		case 'a' : sp1YPosInc+=1; break;		
-		case ' ' : fire=1; break;
-		default:
-		break;
-	}
-	sp1XPos = sp1XPosInc + sp1XPos;
-	sp1YPos = sp1YPosInc + sp1YPos;
-	sp2XPos+=1;
-	
-    sp1_MoveSprPix(spaceship1, &full_screen, 0, sp1XPos, sp1YPos);
-	sp1_MoveSprPix(spaceship2, &full_screen, 0, sp2XPos, sp2YPos);
-    intrinsic_halt();
-    sp1_UpdateNow();
-	
-	// limit the xy position to screen area and "bounce" off edges
-	if (sp1XPos<=-sp1XPosInc) { sp1XPosInc = 1; sp1XPos = 0; }
-	if (sp1XPos>=248-sp1XPosInc) { sp1XPosInc = -1; sp1XPos = 248; }
-	if (sp1YPos<=-sp1YPosInc) { sp1YPosInc = 1; sp1YPos = 0; }
-    if (sp1YPos>=184-sp1YPosInc) { sp1YPosInc = -1; sp1YPos = 184; }
-	
-	if (sp1XPosInc<=-MAX_SPEED_SACESHIP_1) sp1XPosInc = -MAX_SPEED_SACESHIP_1;
-	if (sp1XPosInc>=MAX_SPEED_SACESHIP_1) sp1XPosInc = MAX_SPEED_SACESHIP_1;
-	if (sp1YPosInc<=-MAX_SPEED_SACESHIP_1) sp1YPosInc = -MAX_SPEED_SACESHIP_1;
-	if (sp1YPosInc>=MAX_SPEED_SACESHIP_1) sp1YPosInc = MAX_SPEED_SACESHIP_1;
-	
-	if (fire == 1)
-	{
-		//line(sp1XPos, sp1YPos, sp2XPos, sp2YPos);		
-		line(sp1XPos, sp1YPos, sp1XPos, 0);
-	}
-	fire = 0;
+		printScores(&pss, score, highscore,lives);
+		
+		c = in_inkey();
+		
+		switch (c)
+		{
+			case 'o' : sp1XPosInc-=1; break;
+			case 'p' : sp1XPosInc+=1; break;
+			case 'q' : sp1YPosInc-=1; break;
+			case 'a' : sp1YPosInc+=1; break;		
+			case ' ' : fire=1; break;
+			default:
+			break;
+		}
+		sp1XPos = sp1XPosInc + sp1XPos;
+		sp1YPos = sp1YPosInc + sp1YPos;
+		sp2XPos = sp2XPosInc + sp2XPos;
+		sp2YPos = sp2YPosInc + sp2YPos;
+		
+		sp1_MoveSprPix(spaceship1, &full_screen, 0, sp1XPos, sp1YPos);
+		sp1_MoveSprPix(spaceship2, &full_screen, 0, sp2XPos, sp2YPos);
+		intrinsic_halt();
+		sp1_UpdateNow();
+		
+		// limit the xy position to screen area and "bounce" off edges
+		if (sp1XPos<=-sp1XPosInc) { sp1XPosInc = 1; sp1XPos = 0; }
+		if (sp1XPos>=248-sp1XPosInc) { sp1XPosInc = -1; sp1XPos = 248; }
+		if (sp1YPos<=-sp1YPosInc+16) { sp1YPosInc = 1; sp1YPos = 16; }
+		if (sp1YPos>=184-sp1YPosInc) { sp1YPosInc = -1; sp1YPos = 184; }
+		
+		if (sp2XPos<=-sp2XPosInc) { sp2XPosInc = 4; sp2XPos = 0; }
+		if (sp2XPos>=248-sp1XPosInc) { sp2XPosInc = -4; sp2XPos = 248; }
+		if (sp2YPos<=-sp2YPosInc+16) { sp2YPosInc = 4; sp2YPos = 16; }
+		if (sp2YPos>=184-sp2YPosInc) { sp2YPosInc = -4; sp2YPos = 184; }
+		
+		if (sp1XPosInc<=-MAX_SPEED_SACESHIP_1) sp1XPosInc = -MAX_SPEED_SACESHIP_1;
+		if (sp1XPosInc>=MAX_SPEED_SACESHIP_1) sp1XPosInc = MAX_SPEED_SACESHIP_1;
+		if (sp1YPosInc<=-MAX_SPEED_SACESHIP_1) sp1YPosInc = -MAX_SPEED_SACESHIP_1;
+		if (sp1YPosInc>=MAX_SPEED_SACESHIP_1) sp1YPosInc = MAX_SPEED_SACESHIP_1;
+		
+		if (overWriteLine == 1)
+		{
+			overWriteLine = 0;
+			clearVertline(sp1XPosPrev, sp1YPosPrev, 16);
+			clearVertline(sp1XPosPrev+7, sp1YPosPrev, 16);
+		}
+		
+		if ((fire == 1) && (countdownTillFireAvailable-- == 0))
+		{			
+			vertline(sp1XPos, sp1YPos, 16);
+			vertline(sp1XPos+7, sp1YPos, 16);
+			
+			if ((sp1XPos+7 >= sp2XPos) && (sp1XPos <= sp2XPos+7))
+			{
+				score+=100;
+			}	
+			sp1XPosPrev = sp1XPos;
+			sp1YPosPrev = sp1YPos;
+			overWriteLine = 1;
+			countdownTillFireAvailable = FIRE_CUTTOUT;
+		}
+		fire = 0;
+		// check collision, this doesn't scale well to multiple enemys
+		if ((sp1XPos+7 >= sp2XPos) && (sp1XPos <= sp2XPos+7) &&
+			 (sp1YPos+7 >= sp2YPos) && (sp1YPos <= sp2YPos+7))
+		{
+			lives -= 1;
+			// reset ships positions
+			sp1XPos=10;
+			sp2XPos=100;
+			sp1YPos=160;
+			sp2YPos=20;			
+			if (lives<= 0) 
+			{
+				dead = 1;
+				if (score > highscore) 
+				{
+					highscore = score;						
+				}
+				
+				sp1_SetPrintPos(&pss, 10, 10);   
+				sp1_PrintString(&pss, "Game Over");
+				
+				int x = 0;
+				while(x < INT_MAX)
+				{
+					x++;
+				}
+			}
+		}	
+	  }
   }
 }
